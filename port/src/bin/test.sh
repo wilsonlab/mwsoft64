abff35e008a87920cce19624c7d1303c255ea517
Stuart Layton <slayton@mit.edu>
2012-09-14 13:58:24 -0400

    fixed compilation and test scripts for adextract

--- port/src/bin/test.sh
+++
@@ -1,7 +0,0 @@
-#Test Extraction
-infile='raw.spk';
-out32='out32.tt'
-out64='out64.tt'
-
-adextract -eslen80 -t -probe 0 $infile -o $out32
-./adextract64 -eslen80 -t -probe 0 $infile -o $out64
--- port/src/cluster/build64.sh
+++
@@ -1,5 +0,0 @@
-clear;
-DATE=`/bin/date`;
-CMD='gcc adextract64.c iolib.c -o ../bin/adextract64 -I . -DDATE="date"';
-echo $CMD
-$CMD
