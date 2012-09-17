2548f02f62cbefa3cdc1ea16c2e9ca8eb396d2fc
Stuart Layton <slayton@mit.edu>
2012-09-17 14:03:41 -0400

    fixed bug in script causing wrong file to get compiled

--- port/src/cluster/build_spikeparms.sh
+++ port/src/cluster/build_spikeparms.sh
@@ -2,6 +2,6 @@ clear;
 DATE=`/bin/date`;
 ARCH=`uname -m`;
 
-CMD='gcc spikeparms2_64.c iolib.c -o ../bin/spikeparms/sp2_'$ARCH' -I . -DDATE="date" -lm';
+CMD='gcc spikeparms_64.c iolib.c -o ../bin/spikeparms/sp2_'$ARCH' -I . -DDATE="date" -lm';
 echo $CMD
 $CMD
