8609faab3aaa18ee3fbb6426d0d3d4445f645661
Stuart Layton <slayton@mit.edu>
2012-09-17 17:15:21 -0400

    removed files that aren't going to be ported and are not needed

--- port/src/cluster/build_adextract.sh
+++
@@ -1,6 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-CMD='gcc adextract64.c iolib.c -o ../bin/adextract/adextract_'$ARCH' -I . -DDATE="date"';
-echo $CMD
-$CMD
--- port/src/cluster/build_spikeparms.sh
+++
@@ -1,7 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-
-CMD='gcc spikeparms_64.c iolib.c -o ../bin/spikeparms/sp2_'$ARCH' -I . -DDATE="date" -lm';
-echo $CMD
-$CMD
