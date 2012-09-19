8768c5ace5704578432883fc0da17aad0303cac5
Stuart Layton <slayton@mit.edu>
2012-09-19 15:42:56 -0400

    removed source files from the gh-pages branch

--- scripts/build_adextract.sh
+++
@@ -1,8 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-
-CMD='gcc ../src/extract/adextract.c ../src/iolib.c -o ../tests/adextract_'$ARCH' -I ../src/ -DDATE="date" -lm';
-
-echo $CMD
-$CMD
--- scripts/build_spikeave.sh
+++
@@ -1,8 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-
-CMD='gcc ../src/extract/spikeavgn.c ../src/iolib.c -o ../tests/spikeavg/sa_'$ARCH' -I ../src/ -DDATE="date" -lm';
-
-echo $CMD
-$CMD
