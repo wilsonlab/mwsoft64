550754e38308cf3ec7c22a18157fba18418a5017
Stuart Layton <stuart.layton@gmail.com>
2012-09-19 17:07:27 -0400

    fixed broken build scripts that were pointing to the wrong source files and build destinations

--- scripts/build_adextract.sh
+++ scripts/build_adextract.sh
@@ -2,7 +2,7 @@ clear;
 DATE=`/bin/date`;
 ARCH=`uname -m`;
 
-CMD='gcc ../src/extract/adextract.c ../src/iolib.c -o ../tests/adextract_'$ARCH' -I ../src/ -DDATE="date" -lm';
+CMD='gcc ../src/extract/adextract.c ../src/iolib.c -o ../tests/adextract/adextract_'$ARCH' -I ../src/ -DDATE="date" -lm';
 
 echo $CMD
 $CMD
--- scripts/build_spikeparms.sh
+++ scripts/build_spikeparms.sh
@@ -2,6 +2,6 @@ clear;
 DATE=`/bin/date`;
 ARCH=`uname -m`;
 
-CMD='gcc ../src/extract/spikeparms2.c ../src/iolib.c -o ../build/sp2_'$ARCH' -I ../src/ -DDATE="date" -lm';
+CMD='gcc ../src/extract/spikeparms2.c ../src/iolib.c -o ../test/spikeparms/sp2_'$ARCH' -I ../src/ -DDATE="date" -lm';
 echo $CMD
 $CMD
