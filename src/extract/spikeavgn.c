488593dc3663cc7a605b7efdf03cde73b9881da0
Stuart Layton <slayton@mit.edu>
2012-09-19 13:18:37 -0400

    added code to prevent conversion from i686 to x86_64 architectures

--- src/extract/spikeavgn.c
+++ src/extract/spikeavgn.c
@@ -2720,18 +2720,42 @@ char	line[100];
     if(verbose){
 	DisplayHeader(stderr,header,stheadersize);
     }
-    /*
+    
+/*
     ** compare architectures
     */
     if((GetLocalArchitecture() == GetFileArchitecture(header)) ||
         (GetFileArchitecture(header) == ARCH_UNKNOWN)) {
         convert = 0;
     } else {
-        convert = 0;
-        fprintf(stderr,"Converting data from %s to %s architectures.\n",
-            GetFileArchitectureStr(header),
-            GetLocalArchitectureStr());
+    	if (( GetLocalArchitecture() == 'i686' && GetFileArchitecture(header) != 'x86_64') || ( GetLocalArchitecture() == 'x86_64' && GetFileArchitecture(header) != 'i686'))
+    	{
+    		convert = 0;
+    		fprintf(stderr,"Different architectures %s and %s, but conversion not needed.\n",
+        	    GetFileArchitectureStr(header),
+            	GetLocalArchitectureStr());
+
+    	} else{
+	        convert = 1;
+    	    fprintf(stderr,"Converting data from %s to %s architectures.\n",
+        	    GetFileArchitectureStr(header),
+            	GetLocalArchitectureStr());
+        }
     }
+    /*
+    ** compare architectures
+    */
+    // if((GetLocalArchitecture() == GetFileArchitecture(header)) ||
+    //     (GetFileArchitecture(header) == ARCH_UNKNOWN)) {
+    //     convert = 0;
+    // } else {
+    //     convert = 1;
+    //     fprintf(stderr,"Converting data from %s to %s architectures.\n",
+    //         GetFileArchitectureStr(header),
+    //         GetLocalArchitectureStr());
+    // }
+
+
 
     /*
     ** try to read some parameters from the header
