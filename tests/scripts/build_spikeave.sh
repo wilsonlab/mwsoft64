db655ffed93977c04498417c805a28e7afaf661b
Stuart Layton <slayton@mit.edu>
2012-10-30 12:29:35 -0400

    removed old and unused scripts

--- tests/scripts/build_spikeave.sh
+++
@@ -1,8 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-
-CMD='gcc ../src/extract/spikeavg.c ../src/iolib.c -o ../tests/spikeavg/sa_'$ARCH' -I ../src/ -DDATE="date" -lm';
-
-echo $CMD
-$CMD
--- tests/scripts/build_spikeparms.sh
+++
@@ -1,7 +0,0 @@
-clear;
-DATE=`/bin/date`;
-ARCH=`uname -m`;
-
-CMD='gcc ../src/extract/spikeparms2.c ../src/iolib.c -o ../test/spikeparms/sp2_'$ARCH' -I ../src/ -DDATE="date" -lm';
-echo $CMD
-$CMD
